6db6a3112cc0dc2e8dfd3d081b8a6cce41b036f2
Marc Altmann <40744649+MarcA711@users.noreply.github.com>
2025-11-20 13:44:48 +0100

    feat: implement repeated scans

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 import sys
 from PySide6 import QtCore, QtWidgets, QtGui, QtGraphs, QtGraphsWidgets, QtCharts
+from PySide6.QtCore import QThread, Signal
 import pyqtgraph as pg
 
 import pickle
 from pathlib import Path
 
-from scanner import do_scan
+from scanner import ScanWorker
 
 pg.setConfigOption('background', 'w')
 pg.setConfigOption('foreground', 'k')
@@ -14,12 +15,22 @@ class MyWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
-        self.scan_data = {
-            "current": None
-        }
+        self.scan_data = { "current": None }
+        self.signal_update_data = None
+
+        self.worker_thread = QThread()
+        self.scan_worker = ScanWorker()
+        self.scan_worker.moveToThread(self.worker_thread)
+        self.worker_thread.finished.connect(self.scan_worker.deleteLater)
+        self.scan_worker.update_signal.connect(self.update_signal)
+        self.scan_worker.finished_scan.connect(self.update_scan_data)
+        self.worker_thread.start()
 
         self.model = QtGui.QStandardItemModel()
-        self.model.appendRow(self.new_item("current"))
+        item = self.new_item("current")
+        item.setSelectable(False)
+        # item.setEditable(False)
+        self.model.appendRow(item)
         self.model.itemChanged.connect(self.plot_data)
 
         self.layout = QtWidgets.QVBoxLayout(self)
@@ -49,12 +60,18 @@ class MyWidget(QtWidgets.QWidget):
         self.data_control_box.setLayout(self.data_control_box_layout)
         self.data_button_layout = QtWidgets.QHBoxLayout()
         self.data_control_box_layout.addLayout(self.data_button_layout)
-        self.data_save_button = QtWidgets.QPushButton("Save current")
+        self.data_save_button = QtWidgets.QPushButton()
         self.data_button_layout.addWidget(self.data_save_button)
+        self.data_save_button.setIcon(QtGui.QIcon.fromTheme(QtGui.QIcon.ThemeIcon.DocumentSave))
         self.data_save_button.clicked.connect(self.save_current_data)
-        self.data_load_button = QtWidgets.QPushButton("Load")
+        self.data_load_button = QtWidgets.QPushButton()
         self.data_button_layout.addWidget(self.data_load_button)
+        self.data_load_button.setIcon(QtGui.QIcon.fromTheme(QtGui.QIcon.ThemeIcon.DocumentOpen))
         self.data_load_button.clicked.connect(self.load_data)
+        self.data_del_button = QtWidgets.QPushButton()
+        self.data_button_layout.addWidget(self.data_del_button)
+        self.data_del_button.setIcon(QtGui.QIcon.fromTheme(QtGui.QIcon.ThemeIcon.EditDelete))
+        self.data_del_button.clicked.connect(self.delete_data)
         self.data_list = QtWidgets.QListView()
         self.data_control_box_layout.addWidget(self.data_list)
         self.data_list.setModel(self.model)
@@ -63,16 +80,25 @@ class MyWidget(QtWidgets.QWidget):
         self.properties_layout.addWidget(self.scan_control_box)
         self.scan_control_box_layout = QtWidgets.QVBoxLayout()
         self.scan_control_box.setLayout(self.scan_control_box_layout)
+        self.scan_control_button_layout = QtWidgets.QHBoxLayout()
+        self.data_control_box_layout.addWidget(self.scan_control_button_layout)
 
-        self.button = QtWidgets.QPushButton("Start scan")
-        self.scan_control_box_layout.addWidget(self.button)
-        self.button.clicked.connect(self.scan_wrapper)
+        self.single_scan_button = QtWidgets.QPushButton("Single scan")
+        self.scan_control_button_layout.addWidget(self.single_scan_button)
+        self.single_scan_button.clicked.connect(self.scan_worker.do_single_scan)
+        self.start_scanning_button = QtWidgets.QPushButton("Start scanning")
+        self.scan_control_button_layout.addWidget(self.start_scanning_button)
+        self.start_scanning_button.clicked.connect(self.scan_worker.do_repeated_scan)
+        self.stop_scanning_button = QtWidgets.QPushButton("Stop scanning")
+        self.scan_control_button_layout.addWidget(self.stop_scanning_button)
+        self.stop_scanning_button.clicked.connect(self.stop_scanning)
 
     def new_item(self, text):
         item = QtGui.QStandardItem(text)
         item.setCheckState(QtCore.Qt.CheckState.Checked)
         item.setCheckable(True)
         item.setEditable(False)
+        item.setSelectable(True)
 
         idx = self.model.rowCount()
         color = pg.intColor(idx)
@@ -84,15 +110,19 @@ class MyWidget(QtWidgets.QWidget):
         painter.end()
         item.setIcon(pixmap)
 
-
         return item
-        # self.model.appendRow(item)
 
-    def scan_wrapper(self):
-        result = do_scan()
-
+    def update_scan_data(self, result):
         self.scan_data["current"] = result
+        self.signal_update_data = None
         self.plot_data()
+
+    def update_signal(self, result):
+        self.signal_update_data = result
+        self.plot_data()
+
+    def stop_scanning(self):
+        self.scan_worker._stop = True
 
     def save_current_data(self):
         if self.scan_data["current"] is None:
@@ -113,7 +143,7 @@ class MyWidget(QtWidgets.QWidget):
     def load_data(self):
         path = QtWidgets.QFileDialog.getOpenFileName(filter="*.dat")
         path = Path(path[0])
-        if not(path.exists()):
+        if not(path.is_file()):
             return
         basename = path.stem
 
@@ -122,6 +152,15 @@ class MyWidget(QtWidgets.QWidget):
 
         self.model.appendRow(self.new_item(basename))
         self.plot_data()
+
+    def delete_data(self):
+        sel_indexes = self.data_list.selectedIndexes()
+        print(sel_indexes)
+        for ind in sel_indexes:
+            name = self.model.item(ind.row()).text()
+            self.model.removeRow(ind.row())
+            del self.scan_data[name]
+            self.plot_data()
 
     def plot_data(self):
         self.eit_plot.clear()
@@ -133,11 +172,24 @@ class MyWidget(QtWidgets.QWidget):
    
             if item.checkState() == QtCore.Qt.CheckState.Checked:
                 pen = pg.mkPen(color, width=2)
-                result = self.scan_data[item.text()]
-                if result is None:
-                    continue
-                self.eit_plot.plot(result["voltage"], result["eit_clean"], pen=pen)
+                dataset_name = item.text()
+                result = self.scan_data[dataset_name]
+
+                if dataset_name == "current":
+                    if self.signal_update_data is None and result is None:
+                        continue
+                    elif result is None:
+                        self.signal_plot.plot(self.signal_update_data["voltage"], self.signal_update_data["signal_clean"], pen=pen)
+                        continue
+                    elif self.signal_update_data is not None and result is not None:
+                        self.signal_plot.plot(self.signal_update_data["voltage"], self.signal_update_data["signal_clean"], pen=pen)
+                        len_update_signal_data = len(self.signal_update_data["signal_clean"])
+                        self.signal_plot.plot(result["voltage"][len_update_signal_data:], result["signal_clean"][len_update_signal_data:], pen=pen)
+                        self.eit_plot.plot(result["voltage"], result["eit_clean"], pen=pen)
+                        continue
+
                 self.signal_plot.plot(result["voltage"], result["signal_clean"], pen=pen)
+                self.eit_plot.plot(result["voltage"], result["eit_clean"], pen=pen)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
--- src/scanner.py
+++ src/scanner.py
@@ -1,95 +1,121 @@
 import redpitaya_scpi as scpi
 from numpy import linspace
 
-IP = 'rp-f097bc.local'
-rp = scpi.scpi(IP)
+import matplotlib.pyplot as plt
 
-# # generate values for testing
-# def do_scan():
-#     # parameters for signal generation and acquisition
-#     freq = 1
-#     ampl = 1
-#     offset = 0
+from PySide6.QtCore import QObject, QThread, Signal, Slot
 
-#     decimation_factor = 8192
-#     acq_duration = (decimation_factor * (2**14)) / (125e6)
-#     gen_duration =  1 / freq
-#     gen_acq_frac = gen_duration / acq_duration
+class ScanWorker(QObject):
+    QObject
+    update_signal = Signal(dict)
+    finished_scan = Signal(dict)
 
-#     num_samples_during_gen = round(2**14 * gen_acq_frac)
-#     num_samples_segment = round(num_samples_during_gen/4)
-#     end_seg_1 = 1 * num_samples_segment
-#     end_seg_2 = 3 * num_samples_segment
-#     end_seg_3 = 4 * num_samples_segment
+    def __init__(self):
+        super().__init__()
+        self._stop = False
+        IP = 'rp-f097bc.local'
+        self.rp = scpi.scpi(IP)
 
-def do_scan():
-    # parameters for signal generation and acquisition
-    freq = 1
-    ampl = 1
-    offset = 0
+    def do_single_scan(self):
+        # parameters for signal generation and acquisition
+        freq = 1
+        ampl = 1
+        offset = 0
 
-    decimation_factor = 8192
-    acq_duration = (decimation_factor * (2**14)) / (125e6)
-    gen_duration =  1 / freq
-    gen_acq_frac = gen_duration / acq_duration
+        decimation_factor = 8192
+        acq_duration = (decimation_factor * (2**14)) / (125e6)
+        gen_duration =  1 / freq
+        gen_acq_frac = gen_duration / acq_duration
+        freq_factor = 0.955 # emperical factor to compensate small deviations
 
-    num_samples_during_gen = round(2**14 * gen_acq_frac)
-    num_samples_segment = round(num_samples_during_gen/4)
-    end_seg_1 = 1 * num_samples_segment
-    end_seg_2 = 3 * num_samples_segment
-    end_seg_3 = 4 * num_samples_segment
+        num_samples_during_gen = round(2**14 * gen_acq_frac * freq_factor)
+        num_samples_segment = round(num_samples_during_gen/4)
+        end_seg_1 = 1 * num_samples_segment
+        end_seg_2 = 3 * num_samples_segment
+        end_seg_3 = 4 * num_samples_segment
 
-    # reset generation and acquisition
-    rp.tx_txt('GEN:RST')
-    rp.tx_txt('ACQ:RST')
+        result = { "voltage": linspace(-ampl + offset, ampl + offset, 2*num_samples_segment) }
 
-    # setup generation
-    rp.tx_txt('SOUR1:FUNC TRIANGLE')
-    rp.tx_txt(f'SOUR1:FREQ:FIX {freq}')
-    rp.tx_txt(f'SOUR1:VOLT {ampl}')
-    rp.tx_txt(f'SOUR1:VOLT:OFFS {offset}')
+        # reset generation and acquisition
+        self.rp.tx_txt('GEN:RST')
+        self.rp.tx_txt('ACQ:RST')
 
-    rp.tx_txt('SOUR1:BURS:STAT BURST')
-    rp.tx_txt('SOUR1:BURS:NCYC 1')
+        # setup generation
+        self.rp.tx_txt('SOUR1:FUNC TRIANGLE')
+        self.rp.tx_txt(f'SOUR1:FREQ:FIX {freq}')
+        self.rp.tx_txt(f'SOUR1:VOLT {ampl}')
+        self.rp.tx_txt(f'SOUR1:VOLT:OFFS {offset}')
+        self.rp.tx_txt('SOUR1:PHAS 180')
 
-    # setup Acqusition
-    rp.tx_txt(f'ACQ:DEC {decimation_factor}')
-    rp.tx_txt("ACQ:DATA:Units VOLTS")
-    rp.tx_txt('ACQ:TRig:DLY 8192')
-    rp.tx_txt(f'ACQ:SOUR1:GAIN HV')
-    rp.tx_txt(f'ACQ:SOUR2:GAIN HV')
+        self.rp.tx_txt('SOUR1:BURS:STAT BURST')
+        self.rp.tx_txt('SOUR1:BURS:NCYC 1')
 
-    rp.tx_txt('ACQ:START')
-    # time.sleep(1)
-    rp.tx_txt('ACQ:TRig AWG_NE')
-    rp.tx_txt('OUTPUT1:STATE ON')
-    # time.sleep(1)
-    rp.tx_txt('SOUR1:TRig:INT')
+        # setup Acqusition
+        self.rp.tx_txt(f'ACQ:DEC {decimation_factor}')
+        self.rp.tx_txt("ACQ:DATA:Units VOLTS")
+        self.rp.tx_txt('ACQ:TRig:DLY 8192')
+        self.rp.tx_txt(f'ACQ:SOUR1:GAIN HV')
+        self.rp.tx_txt(f'ACQ:SOUR2:GAIN HV')
 
-    while 1:
-        rp.tx_txt('ACQ:TRig:FILL?')
-        if rp.rx_txt() == '1':
-            break
+        self.rp.tx_txt('ACQ:START')
+        # time.sleep(1)
+        self.rp.tx_txt('ACQ:TRig AWG_NE')
+        self.rp.tx_txt('OUTPUT1:STATE ON')
+        # time.sleep(1)
+        self.rp.tx_txt('SOUR1:TRig:INT')
 
-    rp.tx_txt(f'ACQ:SOUR2:DATA?')
-    data_string = rp.rx_txt()
+        
+        self.rp.tx_txt('ACQ:AXI:SOUR1:Trig:Pos?')
+        signal_trig_pointer = int(self.rp.rx_txt())
 
-    data_string = data_string.strip('{}\n\r').replace("  ", "").split(',')
-    data = list(map(float, data_string))
+        while True:
+            self.rp.tx_txt('ACQ:TRig:FILL?')
+            if self.rp.rx_txt() == '1':
+                break
 
-    result = {
-        "voltage": linspace(-ampl + offset, ampl + offset, 2*num_samples_segment),
-        "eit_clean": list(reversed(data[end_seg_1:end_seg_2])),
-        "eit_split": data[end_seg_2:end_seg_3] + data[:end_seg_1],
-    }
+            self.rp.tx_txt('ACQ:AXI:SOUR1:Write:Pos?')
+            signal_curr_pointer = int(self.rp.rx_txt())
 
-    rp.tx_txt(f'ACQ:SOUR1:DATA?')
-    data_string = rp.rx_txt()
+            num = signal_curr_pointer - signal_trig_pointer
+            num = num + 2**14 if num < 0 else num
+            num += num_samples_segment
+            if num < 0:
+                continue
+            num = num if num < 2*num_samples_during_gen else 2*num_samples_during_gen
 
-    data_string = data_string.strip('{}\n\r').replace("  ", "").split(',')
-    data = list(map(float, data_string))
+            self.rp.tx_txt(f'ACQ:AXI:SOUR1:DATA:Start:N? {(signal_trig_pointer+num_samples_during_gen) % (2**14)},{num}>')
 
-    result["signal_clean"] = list(reversed(data[end_seg_1:end_seg_2]))
-    result["signal_clean"] = data[end_seg_2:end_seg_3] + data[:end_seg_1]
+            data_string = self.rp.rx_txt()
 
-    return result
+            data_string = data_string.strip('{}\n\r').replace("  ", "").split(',')
+            data = list(map(float, data_string))
+            signal_result = {
+                "voltage": result["voltage"][:len(data)],
+                "signal_clean": data
+            }
+            self.update_signal.emit(signal_result)
+
+        self.rp.tx_txt(f'ACQ:SOUR2:DATA?')
+        data_string = self.rp.rx_txt()
+
+        data_string = data_string.strip('{}\n\r').replace("  ", "").split(',')
+        data = list(map(float, data_string))
+
+        result["eit_clean"] = data[end_seg_1:end_seg_2],
+        result["eit_split"] = list(reversed(data[end_seg_2:end_seg_3] + data[:end_seg_1])),
+
+        self.rp.tx_txt(f'ACQ:SOUR1:DATA?')
+        data_string = self.rp.rx_txt()
+
+        data_string = data_string.strip('{}\n\r').replace("  ", "").split(',')
+        data = list(map(float, data_string))
+
+        result["signal_clean"] = data[end_seg_1:end_seg_2]
+        result["signal_split"] = list(reversed(data[end_seg_2:end_seg_3] + data[:end_seg_1]))
+
+        self.finished_scan.emit(result)
+
+    def do_repeated_scan(self):
+        self._stop = True
+        while not self._stop:
+            self.do_single_scan
